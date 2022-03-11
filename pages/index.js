cce9b5a2cfa6e44782a1adf9d7981f874058f2d4
D. Johnson <investordami@gmail.com>
2022-03-10 22:18:47 -0500

    update index

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,8 @@ export default function Home({ allPostsData }) {
           <title>{siteTitle}</title>
         </Head>
         <section className={utilStyles.headingMd}>
-          <p>I am dami a Niagara Based software engineer</p>
           <p>
-            (This is a sample website - you’ll be building a site like this on{' '}
-            <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+            Welcome to my nextjs website
           </p>
         </section>
 
